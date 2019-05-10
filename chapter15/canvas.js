b159cfbf6a4629dc53a3a607d721ec51291c05c1
jiangyuwise <jiangyuwise@gmail.com>
2019-05-10 18:27:34 +0800

    draw a clock

--- chapter15/canvas.js
+++ chapter15/canvas.js
@@ -26,13 +26,36 @@ ctx2.clearRect(40, 40, 30, 30)
 /**
  * 绘制路径
  * arc(x, y, radius, startAngle, endAngle, counterclockwise)
- * startAngle 表示起始角度, endAngle 表示结束角度, counterclockwise 为 true 表示逆时针计算角度
+ * radius 表示半径, startAngle 表示起始角度, endAngle 表示结束角度, counterclockwise 为 true 表示逆时针计算角度, 为 false 表示顺时针计算角度
  *
  * arcTo(x1, y1, x2, y2, radius)
  * 以arc定的点开始, 绘制一条弧线, 按指定的半径穿过 (x1, y1), 到达 (x2, y2)
  *
  * lineTo(x, y)
- * 以arc定
+ * 以arc定的点开始, 绘制一条直线, 到达(x, y)
  */
-// var canvas3 = document.getElementById("canvas3")
-// var ctx3 =
+var canvas3 = document.getElementById("canvas3")
+var ctx3 = canvas3.getContext("2d")
+// 开始路径
+ctx3.beginPath()
+
+// 绘制外圆, 起始弧度为0, 结束弧度为2π
+ctx3.arc(100, 100, 99, 0, 2 * Math.PI, false)
+
+// 移动绘图游标, 在绘制内圆之前, 必需将游标移出外圆, 否则就会绘制出多余的线条
+ctx3.moveTo(194, 100)
+
+// 绘制内圆
+ctx3.arc(100, 100, 94, 0, 2 * Math.PI, false)
+
+// 绘制分针
+ctx3.moveTo(100, 100)
+ctx3.lineTo(100, 15)
+
+// 绘制时针
+ctx3.moveTo(100, 100)
+ctx3.lineTo(25, 100)
+
+// 描边路径
+ctx3.stroke()
+
